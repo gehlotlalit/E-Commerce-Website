6f3fb1baefe8fecb8c359d3f64609f18d69bf80d
Lalit Malviya <gehlotlalit786@gmail.com>
2024-05-18 19:32:27 +0530

    Track Modified

--- src/Components/track/Track.jsx
+++ src/Components/track/Track.jsx
@@ -9,7 +9,7 @@ const Track = () => {
                         <div className="border-2 hover:shadow-xl hover:shadow-gray-200 border-gray-200 bg-gray-600-100 shadow-[inset_0_0_2px_rgba(0,0,0,0.6)] px-4 py-6 rounded-lg"  >
                         <svg className="text-black w-12 h-12 mb-3 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M48 0C21.5 0 0 21.5 0 48V368c0 26.5 21.5 48 48 48H64c0 53 43 96 96 96s96-43 96-96H384c0 53 43 96 96 96s96-43 96-96h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V288 256 237.3c0-17-6.7-33.3-18.7-45.3L512 114.7c-12-12-28.3-18.7-45.3-18.7H416V48c0-26.5-21.5-48-48-48H48zM416 160h50.7L544 237.3V256H416V160zM112 416a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm368-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>
                           <h2 className="title-font font-medium text-lg text-gray-900" >Free Delivery</h2>
-                            <p className="leading-relaxed">Our T-Shirts are 100% made of cotton.
+                            <p className="leading-relaxed"> Get FREE delivery on all items marked with the Prime badge in our Country.
                             </p>
                         </div>
                     </div>
@@ -19,7 +19,7 @@ const Track = () => {
                         <div className="border-2 hover:shadow-xl hover:shadow-gray-200 border-gray-200 bg-gray-600-100 shadow-[inset_0_0_2px_rgba(0,0,0,0.6)] px-4 py-6 rounded-lg"  >
                         <svg className="text-black w-12 h-12 mb-3 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M320 96H192L144.6 24.9C137.5 14.2 145.1 0 157.9 0H354.1c12.8 0 20.4 14.2 13.3 24.9L320 96zM192 128H320c3.8 2.5 8.1 5.3 13 8.4C389.7 172.7 512 250.9 512 416c0 53-43 96-96 96H96c-53 0-96-43-96-96C0 250.9 122.3 172.7 179 136.4l0 0 0 0c4.8-3.1 9.2-5.9 13-8.4zm84 88c0-11-9-20-20-20s-20 9-20 20v14c-7.6 1.7-15.2 4.4-22.2 8.5c-13.9 8.3-25.9 22.8-25.8 43.9c.1 20.3 12 33.1 24.7 40.7c11 6.6 24.7 10.8 35.6 14l1.7 .5c12.6 3.8 21.8 6.8 28 10.7c5.1 3.2 5.8 5.4 5.9 8.2c.1 5-1.8 8-5.9 10.5c-5 3.1-12.9 5-21.4 4.7c-11.1-.4-21.5-3.9-35.1-8.5c-2.3-.8-4.7-1.6-7.2-2.4c-10.5-3.5-21.8 2.2-25.3 12.6s2.2 21.8 12.6 25.3c1.9 .6 4 1.3 6.1 2.1l0 0 0 0c8.3 2.9 17.9 6.2 28.2 8.4V424c0 11 9 20 20 20s20-9 20-20V410.2c8-1.7 16-4.5 23.2-9c14.3-8.9 25.1-24.1 24.8-45c-.3-20.3-11.7-33.4-24.6-41.6c-11.5-7.2-25.9-11.6-37.1-15l0 0-.7-.2c-12.8-3.9-21.9-6.7-28.3-10.5c-5.2-3.1-5.3-4.9-5.3-6.7c0-3.7 1.4-6.5 6.2-9.3c5.4-3.2 13.6-5.1 21.5-5c9.6 .1 20.2 2.2 31.2 5.2c10.7 2.8 21.6-3.5 24.5-14.2s-3.5-21.6-14.2-24.5c-6.5-1.7-13.7-3.4-21.1-4.7V216z"/></svg>
                             <h2 className="title-font font-medium text-lg text-gray-900" >Cash On Delivery</h2>
-                            <p className="leading-relaxed">Our T-Shirts are 100% made of cotton.
+                            <p className="leading-relaxed">Pay on Delivery orders can be paid via cash, via Amazon UPI Scan & Pay.
                             </p>
                         </div>
                     </div>
@@ -33,7 +33,7 @@ const Track = () => {
                             </svg> */}
 
                             <h2 className="title-font font-medium text-lg text-gray-900" >Easy Returns</h2>
-                            <p className="leading-relaxed">Our T-Shirts are 100% made of cotton.
+                            <p className="leading-relaxed"> item is eligible for return within 10 days of delivery. You can avail replacement.
                             </p>
                         </div>
                     </div>
